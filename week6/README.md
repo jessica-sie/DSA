3ea2861b14321d4453edc7d542b32ebaca6d3e1a
jessica-sie <jessica.sie@binus.ac.id>
2022-03-29 16:17:36 +0700

    commit

--- week6/README.md
+++ week6/README.md
@@ -1,5 +1,13 @@
 ### Week 6 forum assignment 
 converting infix to postfix notation with recursion and iteration
 
+##### Base Case 
+stops the recrusion 
+ => when increment is equal to the length of the infix notation -1
+
+##### General Case 
+get to the base case 
+
+
 jessica sie 2502053653
 
---
+++ week6/recurssiveInfixPostfix.cpp
@@ -0,0 +1,20 @@
+// converting infix expression to postfix recursive
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+// recursive function - takes in a char -> decide what to do 
+char ITP(string infix, int len, int i){
+    // base case - stops the recursion when i is at the last character of string 
+    if (i == len -1){
+        //break?
+
+    }
+    // general case - reduced to base case 
+    ITP(c);
+}
+
+int main(){
+
+}
